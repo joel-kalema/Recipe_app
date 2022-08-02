468b972631f36ed00ed651e8f4abf7c1942bbfff
joel <joelkalema63@gmail.com>
2022-08-02 16:05:13 +0200

    Added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'general_shopping_list/index'
+  get 'public_recipes/index'
+  get 'recipes/index'
   devise_for :users
   root 'users#index'
-
+  resources :foods
 end
